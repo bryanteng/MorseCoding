8dd318cc9c2428972afa7c46418e58efeeff0ce4
Bryan Teng <bteng1226@gmail.com>
2019-03-21 14:02:23 -0500

    fixed css and removed unneeded text

--- App.js
+++ App.js
@@ -15,7 +15,6 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Receiver />
       </View>
     );
@@ -26,7 +25,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
   welcome: {
--- components/Receiver.js
+++ components/Receiver.js
@@ -9,43 +9,63 @@ export default class Receiver extends Component {
       }
 
     handleOnChange = (event) =>{
-        this.setState({text: event, translated:wordToMorseCode(event)})
+        this.setState({text: event, translated:wordToMorseCode(event.trim())})
     }
 
     render() {
         return (
-            <View>
-                <Text> Receiver component</Text>
-                <Text>Enter Text to be translated here: </Text>
-                <TextInput style={styles.container} onChangeText={this.handleOnChange}> </TextInput>
-                <Text>{this.state.translated}</Text>
+            <View style={styles.receiver}> 
+                <Text style={styles.text}> Morse Coding</Text>
+                <Text style={styles.text}>Enter Text to be translated here: </Text>
+                <TextInput style={styles.textInput} onChangeText={this.handleOnChange}> </TextInput>
+                <Text style={styles.translated}>{this.state.translated}</Text>
             </View>
         );
     }
 }
 const styles = StyleSheet.create({
-    container: {
+    textInput: {
       justifyContent: 'center',
       alignItems: 'center',
-      backgroundColor: '#F5FCFF',
+      backgroundColor: '#E0E0E0',
+      fontSize: 15,
+      borderRadius: 1,
+      borderWidth: 0.5,
+      borderColor: '#d6d7da',
     },
-    welcome: {
-      fontSize: 20,
+    receiver: {
+        flex: 1,
+        backgroundColor: '#F5FCFF',
+        borderRadius: 1,
+        borderWidth: 0.5,
+        borderColor: '#d6d7da',
+        alignItems: 'stretch'
+    },
+    text: {
+      fontSize: 25,
       textAlign: 'center',
       margin: 10,
     },
-    instructions: {
+    translated: {
+        flex:3,
       textAlign: 'center',
       color: '#333333',
       marginBottom: 5,
+      fontSize: 20,
+      alignItems: "stretch"
     }
 })
 
 function wordToMorseCode(string){
-    alphabet_hash = {
-    "a" : ".-","b" : "-...","c" : "-.-.","d" : "-..","e" : ".","f" : "..-.","g" : "--.","h" : "....","i" : "..","j" : ".---","k" : "-.-", "l" : ".-..","m" : "--","n" : "-.","o" : "---","p" : ".--.","q" : "--.-","r" : ".-.","s" : "...","t" : "-","u" : "..-","v" : "...-","w" : ".--","x" : "-..-","y" : "-.--","z" : "--..","1" : ".----","2" : "..---","3" : "...--","4" : "....-","5" : ".....","6": "-....","7": "--...","8": "---..","9": "----.","0": "-----"
+    let splitPhrase = string.split(" ") 
+    let ret = []
+    let alphabet_hash = {
+      "a" : ".-","b" : "-...","c" : "-.-.","d" : "-..","e" : ".","f" : "..-.","g" : "--.","h" : "....","i" : "..","j" : ".---","k" : "-.-", "l" : ".-..","m" : "--","n" : "-.","o" : "---","p" : ".--.","q" : "--.-","r" : ".-.","s" : "...","t" : "-","u" : "..-","v" : "...-","w" : ".--","x" : "-..-","y" : "-.--","z" : "--..","1" : ".----","2" : "..---","3" : "...--","4" : "....-","5" : ".....","6": "-....","7": "--...","8": "---..","9": "----.","0": "-----"
+      }
+      for(word in splitPhrase){
+        temp = splitPhrase[word].match(/[a-z0-9]/gi)
+        temp ? ret.push(temp.map(letter => alphabet_hash[letter.toLowerCase()]).join(" ")) : null
+      }
+      return ret.join(" / ")
     }
-return string.split(" ").map(word=> word.split("").map(letter => alphabet_hash[letter.toLowerCase()]).join("  ")).join(" / ")
-}
-
 
